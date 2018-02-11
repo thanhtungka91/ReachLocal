917b5b86b782f67d556dc44e7ac13b2d9e94232a
Vo Thanh Tung <thanhtungka91@gmail.com>
2018-02-12 01:20:00 +0900

    initial commit

---
+++ articles/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Source(models.Model): 
+    source_id = models.CharField(max_length=200,null=True)
+    name = models.CharField(max_length=200)
+
+class Article(models.Model):
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    urlToImage = models.CharField(max_length=200)
+    publishedAt = models.DateTimeField('date published')
---
+++ articles/views.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from newsapi import NewsApiClient
+from .models import Article
+from .models import Source
+
+from django.http import HttpResponse
+
+#like the controller 
+def index(request):
+    
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    
+    return render(request, 'articles/index.html', context); 
+
+
+def getArticles(request): 
+    #get articles from api 
+
+    newsapi = NewsApiClient(api_key='')
+    all_articles = newsapi.get_everything(q='bitcoin',
+                                      sources='bbc-news,the-verge',
+                                      domains='bbc.co.uk,techcrunch.com',
+                                      language='en',
+                                      sort_by='publishedAt',
+                                      page=1)
+    
+    # check 
+    if all_articles["status"] == "ok": 
+        articles = all_articles["articles"]
+        for article in articles: 
+            #insert source 
+            source_model = Source() 
+            source_from_api = article["source"]
+            source_model.source_id =  source_from_api["id"]
+            source_model.name = source_from_api["name"]
+            source_model.save(); 
+            #insert Articles 
+            article_model = Article()
+            article_model.source = source_model
+            article_model.author = article["author"]
+            article_model.title = article["title"]
+            article_model.description = article["description"]
+            article_model.publishedAt = article["publishedAt"]
+            article_model.url = article["url"]
+            article_model.urlToImage = article["urlToImage"]
+            article_model.save()
+
+    import pdb; pdb.set_trace()
+
+    pass 
